aeacae2f8d49bd080d80be09497b5b8d96976860
Zara <zarabucknor@yahoo.co.uk>
2023-07-19 15:42:13 +0100

    added another else statment to determine transaction type

--- statement.js
+++ statement.js
@@ -10,32 +10,35 @@ class Statement {
     return `${day}/${month}/${year}`;
   }
 
-  printStatement() {
-    let statement = '';
-    let balance = 0;
+ printStatement() {
+  let statement = '';
+  let balance = 0;
 
-    // Sort transactions by date in ascending order
-    const sortedTransactions = this.transactions.sort((a, b) => new Date(a.date) - new Date(b.date));
+  // Sort transactions by date in ascending order
+  const sortedTransactions = this.transactions.sort((a, b) => new Date(a.date) - new Date(b.date));
 
-    for (const transaction of sortedTransactions) {
-      const { date, amount, type } = transaction;
-      const formattedDate = this.formatDate(date);
+  for (const transaction of sortedTransactions) {
+    const { date, amount, type } = transaction;
+    const formattedDate = this.formatDate(date);
 
-      if (type === 'deposit') {
-        balance += amount;
-        statement += `${formattedDate} || ${amount.toFixed(2)} || || ${balance.toFixed(2)}\n`;
-      } else if (type === 'withdrawal') {
-        balance -= amount;
-        statement += `${formattedDate} || || ${amount.toFixed(2)} || ${balance.toFixed(2)}\n`;
-      }
+    if (type === 'deposit') {
+      balance += amount;
+      statement += `${formattedDate} || ${amount.toFixed(2)} || || ${balance.toFixed(2)}\n`;
+    } else if (type === 'withdrawal') {
+      balance -= amount;
+      statement += `${formattedDate} || || ${amount.toFixed(2)} || ${balance.toFixed(2)}\n`;
+    } else {
+      statement += `${formattedDate} || ${type} || || ${balance.toFixed(2)}\n`;
     }
-
-    // Reverse the statement to get the desired output order
-    const reversedStatement = statement.trim().split('\n').reverse().join('\n');
-    const finalStatement = `date || credit || debit || balance\n${reversedStatement}`;
-    // Create the final statement with the header line
-    return finalStatement.trim();
   }
+
+  // Reverse the statement to get the desired output order
+  const reversedStatement = statement.trim().split('\n').reverse().join('\n');
+  const finalStatement = `date || credit || debit || balance\n${reversedStatement}`;
+  // Create the final statement with the header line
+  return finalStatement.trim();
+}
+
 }
 
 module.exports = Statement;
